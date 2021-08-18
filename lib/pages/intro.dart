c63d13a35ee92abf3af8a4f25786bf9bb72ebe3c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-18 16:12:01 +0700

    fix intro

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:restaurant_app/helper/color_theme.dart';
 import 'package:restaurant_app/helper/size_config.dart';
+import 'package:restaurant_app/helper/theme.dart';
 
 class Intro extends StatefulWidget {
   const Intro({Key? key}) : super(key: key);
@@ -11,6 +12,8 @@ class Intro extends StatefulWidget {
 }
 
 class _IntroState extends State<Intro> {
+  int currentPage = 0;
+
   List<Map<String, String>> contentData = [
     {
       "image": "assets/images/breakfast.svg",
@@ -38,36 +41,73 @@ class _IntroState extends State<Intro> {
           backgroundColor: Colors.white,
         ),
         body: SafeArea(
-          top: true,
           child: SizedBox(
             width: double.infinity,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                    flex: 3,
-                    child: PageView.builder(
-                        itemCount: contentData.length,
-                        itemBuilder: (context, index) => Content(
-                            image: contentData[index]["image"],
-                            text: contentData[index]["text"]))),
-                Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: <Widget>[buildDot()],
-                    ))
-              ],
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Restaurant App",
+                    style: TextStyle(
+                        color: ColorTheme.primary,
+                        fontSize: getProportionateScreenWidth(36),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                      flex: 3,
+                      child: PageView.builder(
+                          onPageChanged: (value) {
+                            setState(() {
+                              currentPage = value;
+                            });
+                          },
+                          itemCount: contentData.length,
+                          itemBuilder: (context, index) => Content(
+                              image: contentData[index]["image"],
+                              text: contentData[index]["text"]))),
+                  Spacer(),
+                  Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: List.generate(contentData.length,
+                                (index) => buildDot(index: index)),
+                          ),
+                          Spacer(
+                            flex: 3,
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            height: getProportionateScreenHeight(56),
+                            child: TextButton(
+                                style: flatButtonStyle,
+                                onPressed: () {},
+                                child: Text("Continue")),
+                          ),
+                          Spacer()
+                        ],
+                      ))
+                ],
+              ),
             ),
           ),
         ));
   }
 
-  Container buildDot() {
-    return Container(
+  AnimatedContainer buildDot({required int index}) {
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 200),
         margin: EdgeInsets.only(right: 5),
         height: 6,
-        width: 6,
+        width: currentPage == index ? 20 : 6,
         decoration: BoxDecoration(
-            color: ColorTheme.primary, borderRadius: BorderRadius.circular(3)));
+            color: currentPage == index
+                ? ColorTheme.primary
+                : ColorTheme.secondaryLight,
+            borderRadius: BorderRadius.circular(3)));
   }
 }
 
@@ -80,16 +120,9 @@ class Content extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Spacer(),
-        Text(
-          "Restaurant App",
-          style: TextStyle(
-              color: ColorTheme.primary,
-              fontSize: getProportionateScreenWidth(36),
-              fontWeight: FontWeight.bold),
-        ),
         Text(
           text!,
+          style: TextStyle(color: ColorTheme.secondary),
           textAlign: TextAlign.center,
         ),
         Spacer(
