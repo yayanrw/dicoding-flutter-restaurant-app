f2f272de8a1807ff69490994a3b1d00b11da48d5
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-22 19:02:46 +0700

    add body sign in

---
+++ lib/pages/sign_in/components/body.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/helper/theme/text_theme.dart';
+import 'package:restaurant_app/pages/sign_in/components/sign_form.dart';
+import 'package:restaurant_app/pages/sign_in/components/social_card.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Text("Welcome Back", style: textTheme().headline4),
+              Text(
+                "Sign in with your email and password \nor continue with social media",
+                style: textTheme().subtitle1,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40),
+              SignForm(),
+              SizedBox(height: 40),
+              socialMedia(),
+              SizedBox(height: 24),
+              noAccountText(context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row noAccountText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Don\'t have an account?'),
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/under_construction');
+              },
+              child: Text('Sign Up')),
+        )
+      ],
+    );
+  }
+
+  Row socialMedia() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SocialCard(
+          icon: "assets/icons/google.svg",
+          press: () {
+            Navigator.pushNamed(context, '/under_construction');
+          },
+        ),
+        SocialCard(
+          icon: "assets/icons/facebook.svg",
+          press: () {
+            Navigator.pushNamed(context, '/under_construction');
+          },
+        ),
+        SocialCard(
+          icon: "assets/icons/twitter.svg",
+          press: () {
+            Navigator.pushNamed(context, '/under_construction');
+          },
+        ),
+      ],
+    );
+  }
+}
