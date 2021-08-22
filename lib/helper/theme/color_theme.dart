38e28d7da6914fc01a3c50d36af4685f8f707b0f
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 06:14:36 +0700

    add color

--- lib/helper/theme/color_theme.dart
+++ lib/helper/theme/color_theme.dart
@@ -4,9 +4,13 @@ class ColorTheme {
   static Color primary = HexColor("#FDBD10");
   static Color primaryDark = HexColor("#FC9D0A");
   static Color primaryLight = HexColor("#fed250");
+  static Color primarySmooth = Color(0xFFFFECDF);
 
   static Color secondary = HexColor("#141414");
   static Color secondaryLight = Color(0xFFD8D8D8);
+  static Color secondaryLight2 = Color(0xFF979797);
+
+  static Color purple = Color(0xFF4A3298);
 }
 
 class HexColor extends Color {
