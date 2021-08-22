0053aa05d6938cf15f602b42ff7a4cf225971753
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 06:16:57 +0700

    add banner

---
+++ lib/pages/home/components/discount_banner.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/helper/theme/color_theme.dart';
+import 'package:restaurant_app/helper/theme/text_theme.dart';
+
+class DiscountBanner extends StatelessWidget {
+  const DiscountBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/under_construction');
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(32), color: ColorTheme.purple),
+          child: Text.rich(TextSpan(
+              text: 'A Summer Surprise\n',
+              style: TextStyle(color: Colors.white),
+              children: [
+                TextSpan(
+                    text: 'Cashback 20%',
+                    style: textTheme(Colors.white).headline4)
+              ])),
+        ),
+      ),
+    );
+  }
+}
