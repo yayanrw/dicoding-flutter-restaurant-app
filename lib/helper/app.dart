92b0a3d42e6b0d8f851036649bcdd174ee05621b
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-22 08:11:24 +0700

    add app

---
+++ lib/helper/app.dart
@@ -0,0 +1,21 @@
+class App {
+  static String appName = 'Restaurant App';
+  static List<Map<String, String>> sliderData = [
+    {
+      "image": "assets/images/breakfast.svg",
+      "text": "Welcome to Restaurant App, Let's shop!",
+    },
+    {
+      "image": "assets/images/hamburger.svg",
+      "text": "Do you want a hamburger?",
+    },
+    {
+      "image": "assets/images/ice_cream.svg",
+      "text": "Or, do you want an ice cream?",
+    },
+    {
+      "image": "assets/images/eating_together.svg",
+      "text": "Let's shop and eating together!",
+    },
+  ];
+}
