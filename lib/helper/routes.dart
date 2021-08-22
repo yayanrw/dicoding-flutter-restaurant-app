5a5df91c561c73883d6d48c0c01d246b24e8188d
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-22 18:09:52 +0700

    mod routes

--- lib/helper/routes.dart
+++ lib/helper/routes.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:restaurant_app/pages/intro/intro.dart';
-import 'package:restaurant_app/pages/login/login.dart';
+import 'package:restaurant_app/pages/other/under_construction.dart';
+import 'package:restaurant_app/pages/sign_in/sign_in.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/intro': (context) => Intro(),
-  '/login': (context) => Login(),
+  '/sign_in': (context) => SignIn(),
+  '/under_construction': (context) => UnderConstruction(),
+  '/home': (context) => UnderConstruction(),
 };
