8c7c0f4b6bb5c400482826df576701149179cab9
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 06:18:08 +0700

    add categorycard

---
+++ lib/pages/home/components/category_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/helper/theme/color_theme.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    Key? key,
+    required this.press,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  final VoidCallback press;
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        width: 64,
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: ColorTheme.primarySmooth,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Icon(
+                  icon,
+                  color: ColorTheme.primaryDark,
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
