54659008cc462331e45c6196b8e5967cef814fc8
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 06:18:34 +0700

    add category

---
+++ lib/pages/home/components/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/helper/app.dart';
+import 'package:restaurant_app/pages/home/components/category_card.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ...List.generate(
+              App.menuCategories.length,
+              (index) => CategoryCard(
+                  press: () {
+                    Navigator.pushNamed(context, '/under_construction');
+                  },
+                  icon: App.menuCategories[index]['icon'],
+                  text: App.menuCategories[index]['text'])),
+        ],
+      ),
+    );
+  }
+}
