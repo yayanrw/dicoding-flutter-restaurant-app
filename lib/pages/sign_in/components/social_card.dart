1499aba411d0f942a934863100f958eb8696c34f
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-22 18:09:21 +0700

    add socialcard

---
+++ lib/pages/sign_in/components/social_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key? key,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  final String icon;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        padding: EdgeInsets.all(10),
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          color: Color(0xFFF5F6F9),
+          shape: BoxShape.circle,
+        ),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
