5cfb87b4b85333417a4314658b45475dce4492a3
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 11:42:04 +0700

    add section title

---
+++ lib/pages/home/components/section_title.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/helper/theme/color_theme.dart';
+import 'package:restaurant_app/helper/theme/text_theme.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.text,
+    required this.press,
+    required this.isMoreable,
+  }) : super(key: key);
+
+  final String text;
+  final bool isMoreable;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: textTheme(ColorTheme.secondary).headline6,
+          ),
+          Visibility(
+            visible: isMoreable,
+            child: GestureDetector(
+                onTap: press,
+                child: Text(
+                  'See more',
+                  style: textTheme(ColorTheme.secondaryLight2).caption,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
