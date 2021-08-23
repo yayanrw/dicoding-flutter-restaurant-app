6e4a6b4041ad494b20ac2f252411ee45a9d946d3
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-23 11:42:15 +0700

    add special offer

---
+++ lib/pages/home/components/special_offer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/pages/home/components/section_title.dart';
+import 'package:restaurant_app/pages/home/components/special_offer_card.dart';
+
+class SpecialOffer extends StatelessWidget {
+  const SpecialOffer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          text: 'Best restaurant',
+          isMoreable: true,
+          press: () {
+            Navigator.pushNamed(context, 'under_construction');
+          },
+        ),
+        SizedBox(height: 16),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                  name: 'Warung',
+                  image: 'assets/images/restaurant.jpg',
+                  numOfMenus: 10,
+                  press: () {}),
+              SpecialOfferCard(
+                  name: 'Warung',
+                  image: 'assets/images/restaurant.jpg',
+                  numOfMenus: 10,
+                  press: () {}),
+              SpecialOfferCard(
+                  name: 'Warung',
+                  image: 'assets/images/restaurant.jpg',
+                  numOfMenus: 10,
+                  press: () {}),
+              SpecialOfferCard(
+                  name: 'Warung',
+                  image: 'assets/images/restaurant.jpg',
+                  numOfMenus: 10,
+                  press: () {}),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
