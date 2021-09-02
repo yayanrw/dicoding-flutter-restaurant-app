e0e7bc284be43b2953efd7340a6025ecd444a458
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-02 10:42:50 +0700

    add detail

--- lib/helper/routes.dart
+++ lib/helper/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:restaurant_app/pages/home/home.dart';
 import 'package:restaurant_app/pages/intro/intro.dart';
 import 'package:restaurant_app/pages/other/under_construction.dart';
+import 'package:restaurant_app/pages/restaurant_detail/restaurant_detail.dart';
 import 'package:restaurant_app/pages/sign_in/sign_in.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -9,4 +10,5 @@ final Map<String, WidgetBuilder> routes = {
   '/sign_in': (context) => SignIn(),
   '/under_construction': (context) => UnderConstruction(),
   '/home': (context) => Home(),
+  '/restaurant_detail': (context) => RestaurantDetail(),
 };
