243984eefb0d96d4b08f6bbe4e8bd7e1f661d2f8
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-10 22:24:50 +0700

    check connections

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/helper/size_config.dart';
 import 'package:restaurant_app/pages/home/components/home_body.dart';
+import 'package:restaurant_app/theme/color_theme.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,11 +14,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late StreamSubscription sub;
+  bool isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    sub = Connectivity().onConnectivityChanged.listen((result) {
+      setState(() {
+        isConnected = (result != ConnectivityResult.none);
+
+        isConnected
+            ? showSnackbar("Connected")
+            : showSnackbar("No internet connections");
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
       body: HomeBody(),
     );
+  }
+
+  showSnackbar(String message) {
+    var snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: ColorTheme.primaryDark),
+      ),
+      backgroundColor: ColorTheme.primarySmooth,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
