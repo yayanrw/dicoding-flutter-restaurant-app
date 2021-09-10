1e38c89b51588b224fa4a28375dbd96990b564ae
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-10 19:19:56 +0700

    move other

--- lib/pages/other/under_construction.dart
+++ lib/pages/other/under_construction.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:restaurant_app/helper/theme/color_theme.dart';
-import 'package:restaurant_app/helper/theme/text_theme.dart';
+import 'package:restaurant_app/theme/color_theme.dart';
+import 'package:restaurant_app/theme/text_theme.dart';
 
 class UnderConstruction extends StatelessWidget {
   const UnderConstruction({Key? key}) : super(key: key);
