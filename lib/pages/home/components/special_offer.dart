9d0fd9400e54897c6d948b95f5184250f920c5bc
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-13 22:26:19 +0700

    special offer provider

--- lib/pages/home/components/special_offer.dart
+++ lib/pages/home/components/special_offer.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
-import 'package:restaurant_app/data/model/cls_restaurant.dart';
 import 'package:restaurant_app/pages/home/components/section_title.dart';
 import 'package:restaurant_app/pages/home/components/special_offer_card.dart';
+import 'package:restaurant_app/provider/restaurant_provider.dart';
 
 class SpecialOffer extends StatefulWidget {
   const SpecialOffer({
@@ -14,12 +15,9 @@ class SpecialOffer extends StatefulWidget {
 }
 
 class _SpecialOfferState extends State<SpecialOffer> {
-  late Future<ClsRestaurant> _restaurant;
-
   @override
   void initState() {
     super.initState();
-    _restaurant = ApiService().fetchAllRestaurant();
   }
 
   @override
@@ -34,46 +32,38 @@ class _SpecialOfferState extends State<SpecialOffer> {
           },
         ),
         SizedBox(height: 16),
-        FutureBuilder(
-            future: _restaurant,
-            builder: (context, AsyncSnapshot<ClsRestaurant> snapshot) {
-              try {
-                var state = snapshot.connectionState;
-                if (state != ConnectionState.done) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  if (snapshot.hasData) {
-                    return Container(
-                      height: 130,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: 5,
-                        itemBuilder: (context, index) {
-                          var restaurant = snapshot.data?.restaurants[index];
-                          return SpecialOfferCard(
-                              name: restaurant!.name,
-                              city: restaurant.city,
-                              image: ApiService.imageDir + restaurant.pictureId,
-                              rating: restaurant.rating,
-                              press: () {
-                                Navigator.pushNamed(
-                                    context, '/restaurant_detail',
-                                    arguments: restaurant);
-                              });
-                        },
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text("No internet connections."));
-                  } else {
-                    return Text('');
-                  }
-                }
-              } catch (e) {
-                return Text('No internet connection');
-              }
-            })
+        Consumer<RestaurantProvider>(builder: (context, state, _) {
+          if (state.state == ResultState.Loading) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state.state == ResultState.HasData) {
+            return Container(
+              height: 130,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  var restaurant = state.result!.restaurants[index];
+                  return SpecialOfferCard(
+                      name: restaurant.name,
+                      city: restaurant.city,
+                      image: ApiService.imageDir + restaurant.pictureId,
+                      rating: restaurant.rating,
+                      press: () {
+                        Navigator.pushNamed(context, '/restaurant_detail',
+                            arguments: restaurant);
+                      });
+                },
+              ),
+            );
+          } else if (state.state == ResultState.NoData) {
+            return Center(child: Text(state.message));
+          } else if (state.state == ResultState.Error) {
+            return Center(child: Text("No internet connections."));
+          } else {
+            return Center(child: Text(''));
+          }
+        }),
       ],
     );
   }
