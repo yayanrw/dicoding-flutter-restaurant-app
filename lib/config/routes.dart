e0d7799b54f1b34e192c14a39b309d67dbfd0667
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-13 10:32:34 +0700

    add search routes

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -4,6 +4,7 @@ import 'package:restaurant_app/pages/home/home.dart';
 import 'package:restaurant_app/pages/intro/intro.dart';
 import 'package:restaurant_app/pages/other/under_construction.dart';
 import 'package:restaurant_app/pages/restaurant_detail/restaurant_detail.dart';
+import 'package:restaurant_app/pages/search/search.dart';
 import 'package:restaurant_app/pages/sign_in/sign_in.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -11,6 +12,7 @@ final Map<String, WidgetBuilder> routes = {
   '/sign_in': (context) => SignIn(),
   '/under_construction': (context) => UnderConstruction(),
   '/home': (context) => Home(),
+  '/search': (context) => Search(),
   '/restaurant_detail': (context) => RestaurantDetail(
       restaurantDetail:
           ModalRoute.of(context)?.settings.arguments as ClsRestaurantElement),
