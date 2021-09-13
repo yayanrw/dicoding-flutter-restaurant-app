a7bec28bfe06056ca57239509f1d01111d8a2455
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-13 10:32:50 +0700

    add fetch search

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:restaurant_app/data/model/cls_restaurant.dart';
 import 'package:restaurant_app/data/model/cls_restaurant_detail.dart';
+import 'package:restaurant_app/data/model/cls_restaurant_search.dart';
 
 class ApiService {
   static final String _baseUrl = 'restaurant-api.dicoding.dev';
@@ -12,7 +13,7 @@ class ApiService {
     if (response.statusCode == 200) {
       return ClsRestaurant.fromJson(json.decode(response.body));
     } else {
-      throw Exception('Failed to load top headlines');
+      throw Exception('Failed to load');
     }
   }
 
@@ -21,7 +22,17 @@ class ApiService {
     if (response.statusCode == 200) {
       return ClsRestaurantDetail.fromJson(json.decode(response.body));
     } else {
-      throw Exception('Failed to load top headlines');
+      throw Exception('Failed to load');
+    }
+  }
+
+  Future<ClsRestaurantSearch> fetchSearchRestaurant(query) async {
+    final response =
+        await http.get(Uri.https(_baseUrl, "/search", {'q': query}));
+    if (response.statusCode == 200) {
+      return ClsRestaurantSearch.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load');
     }
   }
 }
