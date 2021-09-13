7d965e6a1677e6843dcd13b9be9a3dc85f532d9e
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-13 22:26:58 +0700

    restaurant search provider

--- lib/provider/restaurant_search_provider.dart
+++ lib/provider/restaurant_search_provider.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app/data/api/api_service.dart';
-import 'package:restaurant_app/data/model/cls_restaurant.dart';
+import 'package:restaurant_app/data/model/cls_restaurant_search.dart';
 
 enum ResultState { Loading, NoData, HasData, Error }
 
-class RestaurantProvider extends ChangeNotifier {
+class RestaurantSearchProvider extends ChangeNotifier {
   final ApiService apiService;
 
-  RestaurantProvider({required this.apiService}) {
-    fetchAllRestaurant();
+  RestaurantSearchProvider({required this.apiService}) {
+    fetchSearchRestaurant();
   }
 
-  ClsRestaurant? _clsRestaurant;
+  ClsRestaurantSearch? _clsRestaurant;
   String _message = '';
   ResultState? _state;
 
   String get message => _message;
 
-  ClsRestaurant? get result => _clsRestaurant;
+  ClsRestaurantSearch? get result => _clsRestaurant;
 
   ResultState? get state => _state;
 
-  Future<dynamic> fetchAllRestaurant() async {
+  Future<dynamic> fetchSearchRestaurant() async {
     try {
       _state = ResultState.Loading;
       notifyListeners();
-      final restaurant = await apiService.fetchAllRestaurant();
+      final restaurant = await apiService.fetchSearchRestaurant("senja");
       if (restaurant.restaurants.isEmpty) {
         _state = ResultState.NoData;
         notifyListeners();
