4bc7633adc268435ee867185620dd7f2e0f46560
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-17 06:56:55 +0700

    add consumer

--- lib/pages/search/components/search_header.dart
+++ lib/pages/search/components/search_header.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurant_app/helper/size_config.dart';
 import 'package:restaurant_app/pages/home/components/circular_button.dart';
+import 'package:restaurant_app/provider/restaurant_search_provider.dart';
 import 'package:restaurant_app/theme/color_theme.dart';
 
-class SearchHeader extends StatelessWidget {
+class SearchHeader extends StatefulWidget {
   const SearchHeader({Key? key}) : super(key: key);
 
+  @override
+  _SearchHeaderState createState() => _SearchHeaderState();
+}
+
+class _SearchHeaderState extends State<SearchHeader> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -26,16 +33,21 @@ class SearchHeader extends StatelessWidget {
             decoration: BoxDecoration(
                 color: ColorTheme.secondaryLight2.withOpacity(0.1),
                 borderRadius: BorderRadius.circular(16)),
-            child: TextField(
-              autofocus: true,
-              textInputAction: TextInputAction.search,
-              decoration: InputDecoration(
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  hintText: "Search restaurant...",
-                  prefixIcon: Icon(LineIcons.search),
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 24, vertical: 12)),
+            child: Consumer<RestaurantSearchProvider>(
+              builder: (context, state, _) => TextField(
+                autofocus: true,
+                textInputAction: TextInputAction.search,
+                decoration: InputDecoration(
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    hintText: "Search restaurant...",
+                    prefixIcon: Icon(LineIcons.search),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 24, vertical: 12)),
+                onSubmitted: (value) {
+                  state.searchText = value;
+                },
+              ),
             ),
           )
         ],
