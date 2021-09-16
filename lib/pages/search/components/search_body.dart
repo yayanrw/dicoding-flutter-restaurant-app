6f10da382d6e973133d1031da7bae49ec378d9e8
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-17 06:56:46 +0700

    add change notif

--- lib/pages/search/components/search_body.dart
+++ lib/pages/search/components/search_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/data/api/api_service.dart';
 import 'package:restaurant_app/pages/search/components/restaurant_list.dart';
 import 'package:restaurant_app/pages/search/components/search_header.dart';
+import 'package:restaurant_app/provider/restaurant_search_provider.dart';
 
 class SearchBody extends StatefulWidget {
   const SearchBody({Key? key}) : super(key: key);
@@ -14,13 +17,16 @@ class _SearchBodyState extends State<SearchBody> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(height: 24),
-          SearchHeader(),
-          SizedBox(height: 24),
-          RestaurantList()
-        ],
+      child: ChangeNotifierProvider<RestaurantSearchProvider>(
+        create: (context) => RestaurantSearchProvider(apiService: ApiService()),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            SearchHeader(),
+            SizedBox(height: 24),
+            RestaurantList(),
+          ],
+        ),
       ),
     ));
   }
