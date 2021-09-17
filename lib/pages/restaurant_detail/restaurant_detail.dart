9e17c930e185286d0fc9c548c47d3f8d1367a3cb
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-17 21:54:29 +0700

    move api

--- lib/pages/restaurant_detail/restaurant_detail.dart
+++ lib/pages/restaurant_detail/restaurant_detail.dart
@@ -1,53 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/data/api/api_service.dart';
 import 'package:restaurant_app/data/model/cls_restaurant.dart';
 import 'package:restaurant_app/helper/size_config.dart';
 import 'package:restaurant_app/pages/restaurant_detail/components/restaurant_detail_body.dart';
-import 'package:restaurant_app/data/model/cls_restaurant_detail.dart';
 
-class RestaurantDetail extends StatefulWidget {
+class RestaurantDetail extends StatelessWidget {
   final ClsRestaurantElement restaurantDetail;
-
-  const RestaurantDetail({
-    Key? key,
-    required this.restaurantDetail,
-  }) : super(key: key);
-
-  @override
-  _RestaurantDetailState createState() => _RestaurantDetailState();
-}
-
-class _RestaurantDetailState extends State<RestaurantDetail> {
-  late Future<ClsRestaurantDetail> _restaurant;
-
-  @override
-  void initState() {
-    super.initState();
-    _restaurant =
-        ApiService().fetchRestaurantDetail(widget.restaurantDetail.id);
-  }
+  const RestaurantDetail({Key? key, required this.restaurantDetail})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Scaffold(
-        body: FutureBuilder(
-            future: _restaurant,
-            builder: (context, AsyncSnapshot<ClsRestaurantDetail> snapshot) {
-              var state = snapshot.connectionState;
-              if (state != ConnectionState.done) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                if (snapshot.hasData) {
-                  return RestaurantDetailBody(
-                    restaurant: snapshot.data!.restaurant,
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(child: Text("No internet connections"));
-                } else {
-                  return Text('');
-                }
-              }
-            }));
+    return Scaffold(body: RestaurantDetailBody(id: restaurantDetail.id));
   }
 }
