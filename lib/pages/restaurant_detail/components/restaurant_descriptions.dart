ab5c21f72fd2f4e8d6b934cccf44a52c222af079
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-09-17 21:54:03 +0700

    add consumer

--- lib/pages/restaurant_detail/components/restaurant_descriptions.dart
+++ lib/pages/restaurant_detail/components/restaurant_descriptions.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
-import 'package:restaurant_app/data/model/cls_restaurant_detail.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurant_app/pages/restaurant_detail/components/chip_text.dart';
+import 'package:restaurant_app/provider/restaurant_detail_provider.dart';
 import 'package:restaurant_app/theme/color_theme.dart';
 import 'package:restaurant_app/theme/text_theme.dart';
 
 class RestaurantDescriptions extends StatefulWidget {
-  const RestaurantDescriptions({Key? key, required this.restaurant})
-      : super(key: key);
-
-  final ClsRestaurantDetailElement restaurant;
+  const RestaurantDescriptions({Key? key}) : super(key: key);
 
   @override
   _RestaurantDescriptionsState createState() => _RestaurantDescriptionsState();
@@ -21,95 +19,112 @@ class _RestaurantDescriptionsState extends State<RestaurantDescriptions> {
   int _maxLine = 5;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Consumer<RestaurantDetailProvider>(builder: (context, state, _) {
+      if (state.state == ResultState.Loading) {
+        return Center(child: CircularProgressIndicator());
+      } else if (state.state == ResultState.HasData) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Text.rich(TextSpan(children: [
-                  TextSpan(
-                    text: widget.restaurant.name + "\n",
-                    style: textTheme(ColorTheme.secondary, FontWeight.w900)
-                        .headline5,
-                  ),
-                  TextSpan(
-                      text: widget.restaurant.city,
-                      style: textTheme(ColorTheme.secondaryLight2).bodyText2)
-                ])),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  _isFavorite = !_isFavorite;
-                });
-              },
-              child: Container(
-                padding: EdgeInsets.all(16),
-                width: 64,
-                decoration: BoxDecoration(
-                  color: _isFavorite ? Color(0xFFFFE6E6) : Color(0xFFF5F6F9),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Text.rich(TextSpan(children: [
+                      TextSpan(
+                        text: state.result!.restaurant.name + "\n",
+                        style: textTheme(ColorTheme.secondary, FontWeight.w900)
+                            .headline5,
+                      ),
+                      TextSpan(
+                          text: state.result!.restaurant.city,
+                          style:
+                              textTheme(ColorTheme.secondaryLight2).bodyText2)
+                    ])),
                   ),
                 ),
-                child: Icon(
-                  LineIcons.heartAlt,
-                  color: _isFavorite ? Color(0xFFFF4848) : Color(0xFFDBDEE4),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isFavorite = !_isFavorite;
+                    });
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    width: 64,
+                    decoration: BoxDecoration(
+                      color:
+                          _isFavorite ? Color(0xFFFFE6E6) : Color(0xFFF5F6F9),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                      ),
+                    ),
+                    child: Icon(
+                      LineIcons.heartAlt,
+                      color:
+                          _isFavorite ? Color(0xFFFF4848) : Color(0xFFDBDEE4),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: Text(state.result!.restaurant.description,
+                  style: textTheme(ColorTheme.secondaryLight2).bodyText2,
+                  textAlign: TextAlign.justify,
+                  maxLines: _maxLine,
+                  overflow: TextOverflow.ellipsis),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isMore = !_isMore;
+                      if (_isMore) {
+                        setState(() {
+                          _maxLine = 1000;
+                        });
+                      } else {
+                        setState(() {
+                          _maxLine = 5;
+                        });
+                      }
+                    });
+                  },
+                  child: Text(
+                    _isMore ? "See less" : "See more detail",
+                    style: textTheme(ColorTheme.primary, FontWeight.bold)
+                        .bodyText2,
+                  )),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 16, top: 16),
+                child: Container(
+                  height: 32,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: state.result!.restaurant.categories.length,
+                      itemBuilder: (context, index) {
+                        return ChipText(
+                            name: state
+                                .result!.restaurant.categories[index].name);
+                      }),
+                ))
           ],
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-          child: Text(widget.restaurant.description,
-              style: textTheme(ColorTheme.secondaryLight2).bodyText2,
-              textAlign: TextAlign.justify,
-              maxLines: _maxLine,
-              overflow: TextOverflow.ellipsis),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: TextButton(
-              onPressed: () {
-                setState(() {
-                  _isMore = !_isMore;
-                  if (_isMore) {
-                    setState(() {
-                      _maxLine = 1000;
-                    });
-                  } else {
-                    setState(() {
-                      _maxLine = 5;
-                    });
-                  }
-                });
-              },
-              child: Text(
-                _isMore ? "See less" : "See more detail",
-                style: textTheme(ColorTheme.primary, FontWeight.bold).bodyText2,
-              )),
-        ),
-        Padding(
-            padding: EdgeInsets.only(left: 16, top: 16),
-            child: Container(
-              height: 32,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: widget.restaurant.categories.length,
-                  itemBuilder: (context, index) {
-                    return ChipText(
-                        name: widget.restaurant.categories[index].name);
-                  }),
-            ))
-      ],
-    );
+        );
+      } else if (state.state == ResultState.NoData) {
+        return Center(child: Text(state.message));
+      } else if (state.state == ResultState.Error) {
+        return Center(child: Text("No internet connections."));
+      } else {
+        return Center(child: Text(''));
+      }
+    });
   }
 }
