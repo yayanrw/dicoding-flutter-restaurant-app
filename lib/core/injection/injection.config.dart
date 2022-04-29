336a3721d0f928330c1a9093f57db6304443d352
yayanrw <yayanraw@gmail.com>
2022-04-29 14:19:13 +0700

    local data source lazy

--- lib/core/injection/injection.config.dart
+++ lib/core/injection/injection.config.dart
@@ -9,21 +9,23 @@ import 'package:http/http.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../features/data/datasources/db/database_helper.dart' as _i4;
-import '../../features/data/datasources/restaurant_remote_datasource.dart'
+import '../../features/data/datasources/restaurant_local_datasource.dart'
     as _i5;
+import '../../features/data/datasources/restaurant_remote_datasource.dart'
+    as _i6;
 import '../../features/data/repositories/restaurant_repository_impl.dart'
-    as _i7;
-import '../../features/domain/repositories/restaurant_repository.dart' as _i6;
-import '../../features/domain/usecases/get_restaurant_detail.dart' as _i8;
-import '../../features/domain/usecases/get_restaurant_search.dart' as _i9;
-import '../../features/domain/usecases/get_restaurants.dart' as _i10;
+    as _i8;
+import '../../features/domain/repositories/restaurant_repository.dart' as _i7;
+import '../../features/domain/usecases/get_restaurant_detail.dart' as _i9;
+import '../../features/domain/usecases/get_restaurant_search.dart' as _i10;
+import '../../features/domain/usecases/get_restaurants.dart' as _i11;
 import '../../features/presentation/provider/restaurant_detail_notifier.dart'
-    as _i11;
-import '../../features/presentation/provider/restaurant_list_notifier.dart'
     as _i12;
-import '../../features/presentation/provider/restaurant_search_notifier.dart'
+import '../../features/presentation/provider/restaurant_list_notifier.dart'
     as _i13;
-import 'register_module.dart' as _i14; // ignore_for_file: unnecessary_lambdas
+import '../../features/presentation/provider/restaurant_search_notifier.dart'
+    as _i14;
+import 'register_module.dart' as _i15; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -33,23 +35,27 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final registerModule = _$RegisterModule();
   gh.lazySingleton<_i3.Client>(() => registerModule.httpClient);
   gh.lazySingleton<_i4.DatabaseHelper>(() => _i4.DatabaseHelper());
-  gh.lazySingleton<_i5.RestaurantRemoteDataSource>(
-      () => _i5.RestaurantRemoteDataSourceImpl(get<_i3.Client>()));
-  gh.lazySingleton<_i6.RestaurantRepository>(() => _i7.RestaurantRepositoryImpl(
-      remoteDataSource: get<_i5.RestaurantRemoteDataSource>()));
-  gh.lazySingleton<_i8.GetRestaurantDetail>(
-      () => _i8.GetRestaurantDetail(get<_i6.RestaurantRepository>()));
-  gh.lazySingleton<_i9.GetRestaurantSearch>(
-      () => _i9.GetRestaurantSearch(get<_i6.RestaurantRepository>()));
-  gh.lazySingleton<_i10.GetRestaurants>(
-      () => _i10.GetRestaurants(get<_i6.RestaurantRepository>()));
-  gh.factory<_i11.RestaurantDetailNotifier>(
-      () => _i11.RestaurantDetailNotifier(get<_i8.GetRestaurantDetail>()));
-  gh.factory<_i12.RestaurantListNotifier>(
-      () => _i12.RestaurantListNotifier(get<_i10.GetRestaurants>()));
-  gh.factory<_i13.RestaurantSearchNotifier>(
-      () => _i13.RestaurantSearchNotifier(get<_i9.GetRestaurantSearch>()));
+  gh.lazySingleton<_i5.RestaurantLocalDataSource>(() =>
+      _i5.RestaurantLocalDataSourceImpl(
+          databaseHelper: get<_i4.DatabaseHelper>()));
+  gh.lazySingleton<_i6.RestaurantRemoteDataSource>(
+      () => _i6.RestaurantRemoteDataSourceImpl(get<_i3.Client>()));
+  gh.lazySingleton<_i7.RestaurantRepository>(() => _i8.RestaurantRepositoryImpl(
+      remoteDataSource: get<_i6.RestaurantRemoteDataSource>(),
+      localDataSource: get<_i5.RestaurantLocalDataSource>()));
+  gh.lazySingleton<_i9.GetRestaurantDetail>(
+      () => _i9.GetRestaurantDetail(get<_i7.RestaurantRepository>()));
+  gh.lazySingleton<_i10.GetRestaurantSearch>(
+      () => _i10.GetRestaurantSearch(get<_i7.RestaurantRepository>()));
+  gh.lazySingleton<_i11.GetRestaurants>(
+      () => _i11.GetRestaurants(get<_i7.RestaurantRepository>()));
+  gh.factory<_i12.RestaurantDetailNotifier>(
+      () => _i12.RestaurantDetailNotifier(get<_i9.GetRestaurantDetail>()));
+  gh.factory<_i13.RestaurantListNotifier>(
+      () => _i13.RestaurantListNotifier(get<_i11.GetRestaurants>()));
+  gh.factory<_i14.RestaurantSearchNotifier>(
+      () => _i14.RestaurantSearchNotifier(get<_i10.GetRestaurantSearch>()));
   return get;
 }
 
-class _$RegisterModule extends _i14.RegisterModule {}
+class _$RegisterModule extends _i15.RegisterModule {}
--- lib/features/data/datasources/restaurant_local_datasource.dart
+++ lib/features/data/datasources/restaurant_local_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:injectable/injectable.dart';
 import 'package:restaurant_app/core/utils/error/exceptions.dart';
 import 'package:restaurant_app/core/utils/my_strings.dart';
 import 'package:restaurant_app/features/data/datasources/db/database_helper.dart';
@@ -13,6 +14,7 @@ abstract class RestaurantLocalDataSource {
   Future<RestaurantTable?> getRestaurantById(int id);
 }
 
+@LazySingleton(as: RestaurantLocalDataSource)
 class RestaurantLocalDataSourceImpl implements RestaurantLocalDataSource {
   RestaurantLocalDataSourceImpl({required this.databaseHelper});
 
